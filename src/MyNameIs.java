5b54c31e236a1f174eecc226df1fa2ddfa6edb7a
Jos Theeuwen <jostheeuwen@gmail.com>
2013-03-13 17:07:33 +0100

    Removed printName method

--- src/MyNameIs.java
+++ src/MyNameIs.java
@@ -3,11 +3,6 @@ public class MyNameIs
     // Display the name from command line
     public static void main(String args[])
     {
-        printName(args[0]);
-    }
-
-    public static void printName(String name)
-    {
-        System.out.println("Hello, my name is " + name);
+        System.out.println("Hello, my name is " +args[0]);
     }
 }

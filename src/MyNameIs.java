91c47fb4475710f8ba97e2e345b582d355a7566d
Jos Theeuwen <j.theeuwen@exxellence.nl>
2013-03-13 15:41:45 +0100

    Commentaar toegevoegd aan MyNameIs

--- src/MyNameIs.java
+++ src/MyNameIs.java
@@ -1,5 +1,6 @@
 public class MyNameIs
-{  
+{
+        // Display the name from command line
         public static void main(String args[])
         {
            System.out.println("Hello, my name is " + args[0]);
